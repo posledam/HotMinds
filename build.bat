2df129f71ad276d7bc05c7810af23ec8a6af7f91
Aleksey Gallyamov <web@emalio.ru>
2017-03-20 22:02:15 +0700

    update nuget.exe and build.bat

--- build.bat
+++ build.bat
@@ -9,8 +9,11 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
+call tools\nuget.exe restore "src\HotMinds.sln"
+
 REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" "src\HotMinds.sln" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+REM "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" "src\HotMinds.sln" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
